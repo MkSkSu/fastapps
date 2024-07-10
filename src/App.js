35efd2b1338a641973dcbddc75c537d6bd107c4c
oowadaa011@gmail.com <oowadaa011@gmail.com>
2024-07-10 21:26:50 +0900

    add TODO

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <button>TODO: add API RequestButton</button>
         <a
           className="App-link"
           href="https://reactjs.org"
